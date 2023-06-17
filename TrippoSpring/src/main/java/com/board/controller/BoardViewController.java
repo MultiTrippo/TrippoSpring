d972443074cdee5c74c8534231dd4306927c1e13
minjikang0822 <minji1563@gmail.com>
2023-06-17 16:10:16 +0900

    board temp jun 17

--- TrippoSpring/src/main/java/com/board/controller/BoardViewController.java
+++ TrippoSpring/src/main/java/com/board/controller/BoardViewController.java
@@ -1,4 +1,4 @@
-package com.trippo.board;
+package com.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BoardViewController {
 
+	//@RequestMapping(value="/boardList", method=RequestMethod.GET)
 	@GetMapping("/boardList")
 	public String boardList() {
 		return "board/boardList";
