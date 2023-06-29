cbf6bfb36b97756717f15ba2f5105707f69fb3f4
tyty4420@naver.com <tyty4420@naver.com>
2023-06-29 14:43:43 +0900

    google map pin add when we clicked and location log

---
+++ TrippoSpring/src/main/webapp/js/schedule/mapPinAdd.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+ 
+  
+  
+  
+ //var marker; // 선택된 위치에 추가될 핀
+ 
+//// function pinAdd(snapshot) {
+// 	var newPosition = snapshot.val();
+// 	var point = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+// 	heatmap.getData().push(point);
+// }
+ 
+ // 맵 안에서 위치 선택 활성화 함수
+ //function pinAddBtn() {
+// 	if(marker) {
+ 		// 기존 핀 제거
+// 		marker.setMap(null);
+// 	}
+ 	
+ 	// 클릭 이벤트 리스너 등록
+// 	google.maps.event.addListener(map, 'click', function(event) {
+// 		placeMarker(event.latLng); // 클릭한 위치에 핀 추가
+// 		console.log("핀 클릭");
+// 	});
+ 
+// }
+ 
+ // 핀 추가 함수
+ //function placeMarker(location) {
+// 	marker = new google.maps.Marker({
+ 		//position: location,
+ 		//map : map
+	//});
+	
+	// 선택된 위치 정보 활용
+	//var latitude = location.lat(); // 위도
+	//var longgitude = location.lng(); // 경도
+	//console.log("핀 클릭 위도 : " + location);
+	
+//}
+	
+	
+	
