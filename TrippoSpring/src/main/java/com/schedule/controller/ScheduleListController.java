f77fd3aa081ad317d3d2b68275024eafa2719e0d
tyty4420@naver.com <tyty4420@naver.com>
2023-07-06 10:04:18 +0900

    [ schedule ] fix shceduleList

--- TrippoSpring/src/main/java/com/schedule/controller/ScheduleListController.java
+++ TrippoSpring/src/main/java/com/schedule/controller/ScheduleListController.java
@@ -25,49 +25,54 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ScheduleListController {
 	@Autowired
-	private ScheduleService service; 
-	
+	private ScheduleService service;
+
 	@RequestMapping(value="/scheduleList", method = RequestMethod.GET)
 	public String getAllScheduleList(Model model, HttpSession session) {
 		MemberVO mvo= (MemberVO)session.getAttribute("loginUser");
 		model.addAttribute("id", mvo.getId());
-		
+
 		System.out.println("id" + mvo.getId());
-		
+
 		List<ScheduleListVO> list = service.getAllScheduleList(mvo.getId());
 		model.addAttribute("getAllScheduleList", list);
-		
+
 		System.out.println("getList : =" + list);
-		
+
 		return "schedule/trav_list";
 	}
-	
+
 	@PostMapping(value="ScheduleInsert", produces = "application/json")
 	@ResponseBody
-	public ModelMap insertSchedule(Model model, @RequestParam("id") String id, ScheduleListVO s) {
-		System.out.println("#Schedule List controller getId : " + s.getId());
-		System.out.println("#controller getTrav_title : " + s.getTrav_title());
+	public ModelMap insertSchedule(Model model, HttpSession session, ScheduleListVO s) {
 		
+		MemberVO mvo= (MemberVO)session.getAttribute("loginUser");
+		model.addAttribute("id", mvo.getId());
+		
+		System.out.println("#Schedule List controller getId : " + mvo.getId());
+		System.out.println("#controller getTrav_title : " + s.getTrav_title());
+
 		// 소문자, 대문자, 숫자를 조합하여 랜덤한 page_id 생성
 		String pageId = generateRandomPageId();
 		s.setPage_id(pageId);
-		
+		s.setId(mvo.getId());
+
 		int se = service.insertSchedule(s);
 		System.out.println("se :" + se);
 		System.out.println("pageId: " + pageId);
-		
+
 		if(se == 1) {
 			System.out.println("controller : Schedule 입력 성공");
 		} else {
 			System.out.println("controller : Schedule 입력 실패");
 		}
-		
+
 		ModelMap map = new ModelMap();
 		map.put("page_id", s.getPage_id());
-		
+
 		return map;
 	}
-	
+
 	private String generateRandomPageId() {
 		String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 		int length = 5;
@@ -81,44 +86,54 @@ public class ScheduleListController {
 
 		return sb.toString();
 	}
-	
+
 	@PostMapping(value="/scheduleUpdate", produces = "application/json")
 	@ResponseBody
-	public ModelMap updateSchedule(@RequestParam("page_id") String pageId, @RequestParam("id") String id, ScheduleListVO s) {
+	public ModelMap updateSchedule(Model model, @RequestParam("page_id") String pageId, HttpSession session, ScheduleListVO s) {
+		
+		MemberVO mvo = (MemberVO)session.getAttribute("loginUser");
+		model.addAttribute("up-id", mvo.getId());
+		
 		System.out.println("#List Controller page_id" + s.getPage_id());
 		System.out.println("#List Controller id" + s.getId());
 		
+		s.setId(mvo.getId());
+		
 		int se = service.updateSchedule(s);
 		System.out.println("#se : " + se);
-		
+
 		if(se == 1) {
 			System.out.println("controller : Schedule 수정 성공");
 		} else {
 			System.out.println("controller : Schedule 수정 실패");
 		}
-		
+
 		ModelMap map = new ModelMap();
 		map.put("page_id", s.getPage_id());
 		return map;
 	}
-	
+
 	@PostMapping(value="/scheduleDelete", produces = "application/json")
 	@ResponseBody
-	public ModelMap deleteSchedule(@RequestParam("page_id") String pageId, @RequestParam("id") String id, ScheduleListVO s) {
+	public ModelMap deleteSchedule(Model model, @RequestParam("page_id") String pageId, HttpSession session, ScheduleListVO s) {
 		System.out.println("#List Controller page_id : " + s.getPage_id());
 		
+		MemberVO mvo = (MemberVO)session.getAttribute("loginUser");
+		model.addAttribute("id", mvo.getId());
+
+		s.setId(mvo.getId());
 		int se = service.deleteSchedule(s);
 		System.out.println("#se : " + se);
-		
+
 		if(se == 1) {
 			System.out.println("controller : Schedule 삭제 성공");
 		} else {
 			System.out.println("controller : Schedule 삭제 실패");
 		}
-		
+
 		ModelMap map = new ModelMap();
 		map.put("page_id", s.getPage_id());
 		return map;
 	}
-	
+
 }
