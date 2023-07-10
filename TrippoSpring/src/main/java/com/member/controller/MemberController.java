d74940e42b604928f5947ddb583ac0df70672661
minjikang0822 <minji1563@gmail.com>
2023-07-10 15:52:03 +0900

    comments error fixed 003

--- TrippoSpring/src/main/java/com/member/controller/MemberController.java
+++ TrippoSpring/src/main/java/com/member/controller/MemberController.java
@@ -17,98 +17,99 @@ import com.member.vo.MemberVO;
 
 @Controller
 public class MemberController {
-	
-	@Autowired
-	private MemberService service;
-	
-	
+   
+   @Autowired
+   private MemberService service;
+   
+   
 
-	
-	
-	/** ȸ�����Ծ�� ������ ���� */
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signUp() {
-		
-		return "member/signup";
-	}
-	
-	/** ȸ������ �Ϸ��ư Ŭ���� */
-	@RequestMapping(value = "/signupComplete", method = RequestMethod.POST)
-	public String signUpComplete(@ModelAttribute MemberVO member) {
-		
-		System.out.println("controller: signUpComplete(member)");
-		System.out.println(member);
-		
-		int n = service.signUpComplete(member);
-		if(n==1) {
-			System.out.println("ȸ������ �Ϸ�");
-		}else {
-			System.out.println("ȸ������ ����");
-		}
-		
-		return "redirect:login";
-	}
-	
-	/** ���̵� �ߺ�Ȯ�� ��ư Ŭ���� */
-	@ResponseBody
-	@PostMapping("/idCheck")
-	public int idCheck(MemberVO member) {
-		
-		System.out.println("controller: idCheck(member)");
-		
-		return service.idCheck(member);
-	}
-	
-	/** �̸��� �ߺ�Ȯ�� ��ư Ŭ���� */
-	@ResponseBody
-	@PostMapping("/emailCheck")
-	public int emailCheck(MemberVO member) {
-		
-		System.out.println("controller: emailCheck(member)");
-		
-		return service.emailCheck(member);
-	}
-	
-	
-	
-	
-	/** �α��������� ���� */
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
-		
-		return "member/login";
-	}
-	
-	/** �α����ϱ� ��ư Ŭ�� */
-	@RequestMapping(value = "/loginComplete", method = RequestMethod.POST)
-	public String loginComplete(@ModelAttribute MemberVO member, HttpServletRequest req, RedirectAttributes rttr) {
-		
-		//view���� �Է��� ���� ����
-		System.out.println("controller���� getId: "+member.getId());
-		System.out.println("controller���� getPassword: "+member.getPassword()); 
-		
-		HttpSession session = req.getSession();
-		MemberVO loginUser = service.loginComplete(member);
-		
-		if(loginUser == null) {
-			//�α��� ����
-			int result = 0;
-			rttr.addFlashAttribute("result", result);
-			return "redirect:login";			
-		}else {
-			//�α��� ����
-			session.setAttribute("loginUser", loginUser);
-			return "redirect:boardList";
-		}
-		
-	}
-	
-	/** �α׾ƿ� ��ư Ŭ�� */
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(HttpSession session) {
-		session.invalidate();
-		
-		return "redirect:login";
-	}
+   
+   
+   /** 회원가입양식 페이지 접속 */
+   @RequestMapping(value = "/signup", method = RequestMethod.GET)
+   public String signUp() {
+      
+      return "member/signup";
+   }
+   
+   /** 회원가입 완료버튼 클릭시 */
+   @RequestMapping(value = "/signupComplete", method = RequestMethod.POST)
+   public String signUpComplete(@ModelAttribute MemberVO member) {
+      
+      System.out.println("controller: signUpComplete(member)");
+      System.out.println(member);
+      
+      int n = service.signUpComplete(member);
+      if(n==1) {
+         System.out.println("회원가입 완료");
+      }else {
+         System.out.println("회원가입 실패");
+      }
+      
+      return "redirect:login";
+   }
+   
+   /** 아이디 중복확인 버튼 클릭시 */
+   @ResponseBody
+   @PostMapping("/idCheck")
+   public int idCheck(MemberVO member) {
+      
+      System.out.println("controller: idCheck(member)");
+      
+      return service.idCheck(member);
+   }
+   
+   /** 이메일 중복확인 버튼 클릭시 */
+   @ResponseBody
+   @PostMapping("/emailCheck")
+   public int emailCheck(MemberVO member) {
+      
+      System.out.println("controller: emailCheck(member)");
+      
+      return service.emailCheck(member);
+   }
+   
+   
+   
+   
+   /** 로그인페이지 접속 */
+   @RequestMapping(value = "/login", method = RequestMethod.GET)
+   public String login() {
+      
+      return "member/login";
+   }
+   
+   /** 로그인하기 버튼 클릭 */
+   @RequestMapping(value = "/loginComplete", method = RequestMethod.POST)
+   public String loginComplete(@ModelAttribute MemberVO member, HttpServletRequest req, RedirectAttributes rttr) {
+      
+      //view에서 입력한 값이 들어옴
+      System.out.println("controller에서 getId: "+member.getId());
+      System.out.println("controller에서 getPassword: "+member.getPassword()); 
+      
+      HttpSession session = req.getSession();
+      MemberVO loginUser = service.loginComplete(member);
+      
+      if(loginUser == null) {
+         //로그인 실패
+         int result = 0;
+         rttr.addFlashAttribute("result", result);
+         return "redirect:login";         
+      }else {
+         //로그인 성공
+         session.setAttribute("loginUser", loginUser);
+         
+         return "redirect:boardList";
+      }
+      
+   }
+   
+   /** 로그아웃 버튼 클릭 */
+   @RequestMapping(value = "/logout", method = RequestMethod.GET)
+   public String logout(HttpSession session) {
+      session.invalidate();
+      
+      return "redirect:login";
+   }
 
 }
